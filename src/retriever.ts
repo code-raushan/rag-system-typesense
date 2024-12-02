a8a804da2766c37f1b3eeb0c712c9d20ec3f5ae4
Raushan Kumar <raushan.development@gmail.com>
2024-12-02 14:19:20 +0530

    add: retriever from the typesense index

--- src/retriever.ts
+++ src/retriever.ts
@@ -1,11 +1,11 @@
 import { typesense } from ".";
 
 export async function retrieveCourses() {
-    const result = await typesense.collections("course").documents().search({
+    const result = await typesense.collections<{ id: string, title: string, subtitle: string, description: string }>("course").documents().search({
         q: "aws",
         query_by: "title,subtitle,description",
     });
-    console.dir(result?.hits?.[0]?.document, { depth: Infinity });
+    console.dir(result?.hits?.[0]?.document.id, { depth: Infinity });
 }
 
 retrieveCourses();
